d46e80b7634bbea87660ae479340e1d7d7818179
Joshua Echeverri <josheche@gmail.com>
2015-05-20 11:29:08 -0400

    began generating sessisons controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
+  resource :sessions
   resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
